6b875da49709f143dbbd39de7ff9f64d7a4c158b
al222jf <al222jf@student.lnu.se>
2014-09-29 18:03:39 +0200

    tried some more with the ifs

--- 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -15,14 +15,11 @@ namespace _1DV402.S2.L1A
 
         public void Initialize()
         {
-            int zero = 0;
-            _count = zero;
+            _count = 0;
            
-            if (_count == 0)
-            {
-                Random nr = new Random();
-                _number = nr.Next(1, 101);
-            }
+            Random nr = new Random();
+            _number = nr.Next(1, 101);
+            
         }
 
         public bool MakeGuess(int number)
@@ -31,29 +28,42 @@ namespace _1DV402.S2.L1A
             {
                 throw new ArgumentOutOfRangeException();
             }
-            if (number < _number)
+            if (_count < MaxNumberOfGuesses)
             {
-                Console.WriteLine("{0} är för lågt. Du har {1} gissningar kvar.", 7 - number, _count);
-            }
-            if (number > _number)
-            {
-                Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar.", number, 7 - _count);
-            }
+                if (number > _number)
+                {
+                    Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar.", number, (MaxNumberOfGuesses - _count));
+                }
+                _count++;
 
-            if (_count == MaxNumberOfGuesses)
-            {
-                Console.WriteLine("Det hemliga talet är {0}", _number);
-            }
-
-            if (number == _number)
-            {
-                Console.Write("RÄTT GISSAT. Du klarade det på {0} försök", _count );
-                return true;
+                if (number < _number)
+                {
+                    Console.WriteLine("{0} är för lågt. Du har {1} gissningar kvar.", number, (MaxNumberOfGuesses - _count));
+                }
+                if (number == _number)
+                {
+                    Console.Write("RÄTT GISSAT. Du klarade det på {0} försök", _count);
+                    return true;
+                }
+                if (_count == MaxNumberOfGuesses)
+                {
+                    Console.WriteLine("Det hemliga talet är {0}", _number);
+                }
+                return false;
             }
             else
             {
-                return false;
+                throw new ApplicationException();
             }
+
+
+            
+           
+            
+
+            
+
+           
         }
 
         public SecretNumber()
