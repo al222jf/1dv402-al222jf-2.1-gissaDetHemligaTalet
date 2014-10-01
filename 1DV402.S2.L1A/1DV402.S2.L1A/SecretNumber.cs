9aa822e77385a21234613aea4985c2e07bd7d04c
al222jf <al222jf@student.lnu.se>
2014-10-01 14:53:55 +0200

    teachers edit

--- 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -9,9 +9,14 @@ namespace _1DV402.S2.L1A
     public class SecretNumber
     {
         public const int MaxNumberOfGuesses = 7;
+
         private int _count;
         private int _number;
 
+        public SecretNumber()
+        {
+            Initialize();
+        }
 
         public void Initialize()
         {
@@ -20,63 +25,52 @@ namespace _1DV402.S2.L1A
            
             Random nr = new Random();
             _number = nr.Next(1, 101);
-            
         }
 
         public bool MakeGuess(int number)
         {
-            //To be sure the number is between 1 and a 100
+            if (_count >= MaxNumberOfGuesses)
+            {
+                //If count is 7 or above
+                throw new ApplicationException();
+            }
+                
+             //To be sure the number is between 1 and a 100
             if (number > 100 || number < 1)
             {
                 throw new ArgumentOutOfRangeException();
             }
 
-            //As long as count is smaller than 7,
-            if (_count < MaxNumberOfGuesses)
-            {
-                //Counts every guess
-                _count++;
+            //As long as count is smaller than 7
 
-                //If the number is above the secret number
-                if (number > _number)
-                {
-                    Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar.", number, (MaxNumberOfGuesses - _count));
-                }
-               
+            //Counts every guess
+            _count++;
+
+            //If you guess right, will also return true
+            if (number == _number)
+            {
+                Console.Write("RÄTT GISSAT. Du klarade det på {0} försök", _count);
+                return true;
+            }
+
+            //If the number is above the secret number
+            if (number > _number)
+            {
+                Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar.", number, (MaxNumberOfGuesses - _count));
+            }
+            else 
+            {
                 //If the number is below the secret number
-                if (number < _number)
-                {
-                    Console.WriteLine("{0} är för lågt. Du har {1} gissningar kvar.", number, (MaxNumberOfGuesses - _count));
-                }
-
-                //If you guess right, will also return true
-                if (number == _number)
-                {
-                    Console.Write("RÄTT GISSAT. Du klarade det på {0} försök", _count);
-                    return true;
-                }
-
-                //If you have used up all your guesses
-                if (_count == MaxNumberOfGuesses)
-                {
-                    Console.WriteLine("Det hemliga talet är {0}", _number);
-                }
-                return false;
+                Console.WriteLine("{0} är för lågt. Du har {1} gissningar kvar.", number, (MaxNumberOfGuesses - _count));
             }
-            else
+
+            //If you have used up all your guesses
+            if (_count == MaxNumberOfGuesses)
             {
-                //If count is 7 or above
-                throw new ApplicationException();
+                Console.WriteLine("Det hemliga talet är {0}", _number);
             }
-        }
 
-        public SecretNumber()
-        {
-            Initialize();
+            return false;
         }
-        
-        
     }
-
-    
 }
